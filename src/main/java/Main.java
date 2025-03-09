c3be85f160467ad4cd6a4fdbe03cec5c41a88f7b
김정민 <raon37910@gmail.com>
2025-03-09 13:50:37 +0900

    Initial Commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 3. 9.
+ */
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Java Version: " + System.getProperty("java.version"));
+	}
+}
