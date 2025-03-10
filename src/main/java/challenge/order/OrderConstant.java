91bd05af14f14a4785eb0b7e4357d1dff13ec56c
김정민 <raon37910@gmail.com>
2025-03-10 11:54:00 +0900

    add 주문 상수 추가

---
+++ src/main/java/challenge/order/OrderConstant.java
@@ -0,0 +1,10 @@
+package challenge.order;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 3. 10.
+ */
+public class OrderConstant {
+	public static final Integer ORDER = 1;
+	public static final Integer NOT_ORDER = 2;
+}
