4456b2cca2e2575d4d5c0ca39106b1d4666f9724
김정민 <raon37910@gmail.com>
2025-03-10 11:11:50 +0900

    refactor Constant 클래스 추가(Menu, MenuItem)

---
+++ src/main/java/challenge/menu/MainMenuConstant.java
@@ -0,0 +1,9 @@
+package challenge.menu;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 3. 10.
+ */
+public class MainMenuConstant {
+	public static final Integer MAIN_MENU_EXIT = 0;
+}
---
+++ src/main/java/challenge/menu/SelectMenuItemConstant.java
@@ -0,0 +1,9 @@
+package challenge.menu;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 3. 10.
+ */
+public class SelectMenuItemConstant {
+	public static final Integer SELECT_MENU_EXIT = 0;
+}
