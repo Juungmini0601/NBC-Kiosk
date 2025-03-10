361ba277113477f20854463e41181f2b184a4d1d
김정민 <raon37910@gmail.com>
2025-03-10 11:53:52 +0900

    add 카트 상수 추가

---
+++ src/main/java/challenge/cart/CartConstant.java
@@ -0,0 +1,12 @@
+package challenge.cart;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 3. 10.
+ *
+ * 카트프로세스 관련 상수 저장용 클래스
+ */
+public class CartConstant {
+	public static final Integer ADD_CART = 1;
+	public static final Integer NOT_ADD_CART = 2;
+}
