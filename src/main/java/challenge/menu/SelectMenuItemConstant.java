6499b98733c7daa664f375970bfcb3b52698345e
김정민 <raon37910@gmail.com>
2025-03-10 11:54:23 +0900

    docs 메뉴 아이템 선택 상수 주석 추가

--- src/main/java/challenge/menu/SelectMenuItemConstant.java
+++ src/main/java/challenge/menu/SelectMenuItemConstant.java
@@ -3,6 +3,8 @@ package challenge.menu;
 /**
  * @author    : kimjungmin
  * Created on : 2025. 3. 10.
+ *
+ * MenuItem 선택 프로세스에 사용되는 상수 클래스
  */
 public class SelectMenuItemConstant {
 	public static final Integer SELECT_MENU_EXIT = 0;
