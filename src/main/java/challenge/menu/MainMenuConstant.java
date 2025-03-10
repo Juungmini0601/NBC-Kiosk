1780f8d6e253ecb801c65dee4c5850033f30c393
김정민 <raon37910@gmail.com>
2025-03-10 11:54:34 +0900

    docs 메인 메뉴 선택 상수 주석 추가

--- src/main/java/challenge/menu/MainMenuConstant.java
+++ src/main/java/challenge/menu/MainMenuConstant.java
@@ -3,7 +3,11 @@ package challenge.menu;
 /**
  * @author    : kimjungmin
  * Created on : 2025. 3. 10.
+ *
+ * 메인 메뉴 선택에 사용되는 상수 클래스
  */
 public class MainMenuConstant {
 	public static final Integer MAIN_MENU_EXIT = 0;
+	public static final Integer ORDER = 4;
+	public static final Integer ORDER_CANCEL = 5;
 }
