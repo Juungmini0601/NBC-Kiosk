8e925d845c1113631ca46c2d2fd6aee24ffb6a2a
김정민 <raon37910@gmail.com>
2025-03-11 20:52:45 +0900

    add OrderCancelCommand

---
+++ src/main/java/challenge/level2/command/OrderCancelCommand.java
@@ -0,0 +1,29 @@
+package challenge.level2.command;
+
+import challenge.level2.cart.Cart;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 3. 11.
+ */
+public class OrderCancelCommand implements Command {
+
+	private final Cart cart;
+
+	public OrderCancelCommand(Cart cart) {
+		this.cart = cart;
+	}
+
+	@Override
+	public void execute() {
+		cart.clearCart();
+		Printer.printCancelOrderMessage();
+	}
+
+	private static class Printer {
+		public static void printCancelOrderMessage() {
+			System.out.println("주문이 취소 되었습니다.");
+			System.out.println("장바구니를 비웁니다.\n");
+		}
+	}
+}
