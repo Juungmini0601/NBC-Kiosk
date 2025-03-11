5159195fe6765e1719320096ac2c7d601800be4a
김정민 <raon37910@gmail.com>
2025-03-11 20:53:29 +0900

    add ScannerHolder

---
+++ src/main/java/challenge/level2/util/ScannerHolder.java
@@ -0,0 +1,18 @@
+package challenge.level2.util;
+
+import java.util.Scanner;
+
+/**
+ * @author    : kimjungmin
+ * Created on : 2025. 3. 11.
+ *
+ * Scanner를 어플리케이션 전역에서 사용하기 위한 유틸 클래스
+ */
+public final class ScannerHolder {
+	public static Scanner sc = new Scanner(System.in);
+
+	// ScannerHolder 인스턴스 생성 금지 및 상속 불가능
+	private ScannerHolder() {
+
+	}
+}
